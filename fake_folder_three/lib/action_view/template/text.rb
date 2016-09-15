9cf1d6aab24dbc10d8b445c2530624371a97ec35
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2016-09-15 13:48:20 -0400

    add folder

---
+++ fake_folder_three/lib/action_view/template/text.rb
@@ -0,0 +1,31 @@
+module ActionView #:nodoc:
+  # = Action View Text Template
+  class Template
+    class Text #:nodoc:
+      attr_accessor :type
+
+      def initialize(string, type = nil)
+        @string = string.to_s
+        @type   = Types[type] || type if type
+        @type ||= Types[:text]
+      end
+
+      def identifier
+        "text template"
+
+      end
+
+      def inspect
+        "text template"
+      end
+
+      def render(*args)
+        to_str
+      end
+
+      def formats
+        [@type.respond_to?(:ref) ? @type.ref : @type.to_s]
+      end
+    end
+  end
+end
